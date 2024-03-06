e027178f9ab4e9b36e465240318d8ab46f6556c6
BattlerHaru <kronk.sdance@gmail.com>
2024-03-06 00:25:36 -0600

    gallery component compatibily for next to prev buttons and touch mode

---
+++ README.md
@@ -0,0 +1,7 @@
+## Portfolio
+
+### Resources (src/assets/icons/)
+
+sections icons: https://tabler.io/icons/
+skills icons: https://devicon.dev/
+social icons: https://svgl.vercel.app/
--- src/components/content/project/ProjectGallery/ProjectGallery.ts
+++ src/components/content/project/ProjectGallery/ProjectGallery.ts
@@ -1,39 +1,104 @@
 const images: NodeListOf<HTMLImageElement> =
     document.querySelectorAll(".gallery-img");
+
 const modal = document.getElementById("modalContainer") as HTMLDialogElement;
-const closeButton = document.getElementById(
-    "modalBtnClose",
-) as HTMLButtonElement;
 const modalContent = document.getElementById(
     "modalContent",
 ) as HTMLButtonElement;
+const modalImg = document.getElementById("modalImg") as HTMLImageElement;
+
+const closeButton = document.getElementById(
+    "modalBtnClose",
+) as HTMLButtonElement;
+const prevButton = document.getElementById(
+    "modalBtnPrev",
+) as HTMLButtonElement;
+const nextButton = document.getElementById(
+    "modalBtnNext",
+) as HTMLButtonElement;
+
+let imageCollection: string[] = [];
+let imageCurrent: string = "";
+const regexId = /-[^-]*$/;
+const regexSrc = /^.*\//;
+
+let startX: number;
+let startY: number;
+
+
 
 images.forEach((image) => {
     image.addEventListener("click", () => {
-        openModal(image.src);
+        openModal(image.src, image.id);
     });
 });
 
-const openModal = (src: string) => {
-    const modalImg = document.getElementById("modalImg") as HTMLImageElement;
+const getImageCollection = (id: string) => {
+    imageCollection = [];
+    imageCurrent = id;
+    images.forEach((image) => {
+        if (image.id.includes(id.replace(regexId, ""))) {
+            imageCollection.push(image.src);
+        }
+    })
+}
 
+const controlImageCollection = (direction: "next" | "prev") => {
+    let indexControl: number = 0;
+    const currentIndex = imageCollection.findIndex(imageSrc => imageSrc.includes(imageCurrent.replace(regexSrc, "")));
+
+
+    if (direction === "next") {
+        indexControl = (imageCollection.length - 1) === currentIndex ? 0 : (currentIndex + 1);
+    } else if (direction === "prev") {
+        indexControl = currentIndex === 0 ? (imageCollection.length - 1) : (currentIndex - 1);
+    }
+    modalImg.src = imageCollection[indexControl];
+    imageCurrent = modalImg.src.replace(regexSrc, "")
+}
+
+const openModal = (src: string, id: string) => {
     if (modalImg) {
+        getImageCollection(id);
         modalImg.src = src;
         modal.showModal();
     }
 };
+
+prevButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    controlImageCollection("prev")
+});
+
+nextButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    controlImageCollection("next")
+});
 
 closeButton.addEventListener("click", (event) => {
     event.preventDefault();
     modal.close();
 });
 
-// window.addEventListener("orientationchange", () => {
-//   if (window.matchMedia("(orientation: landscape)").matches) {
-//     // La pantalla está en modo apaisado
-//     console.log("La pantalla está en modo apaisado (-)");
-//   } else {
-//     // La pantalla está en modo vertical
-//     console.log("La pantalla está en modo vertical (I)");
-//   }
-// });
+
+// test touch
+modal.addEventListener("touchstart", (event) => {
+    startX = event.touches[0].clientX;
+    startY = event.touches[0].clientY;
+});
+
+
+modal.addEventListener("touchend", (event) => {
+    const endX = event.changedTouches[0].clientX;
+    const endY = event.changedTouches[0].clientY;
+    const deltaX = endX - startX;
+    const deltaY = endY - startY;
+
+    if (Math.abs(deltaX) > Math.abs(deltaY) && deltaX > 0) {
+        prevButton.click();
+    }
+
+    else if (Math.abs(deltaX) > Math.abs(deltaY) && deltaX < 0) {
+        nextButton.click();
+    }
+});
